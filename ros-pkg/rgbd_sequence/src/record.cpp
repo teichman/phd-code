b4e75c6d2f8907e202611bb08b003776bbc9430c
Alex Teichman <teichman@stanford.edu>
2012-03-03 17:01:37 -0800

    Removing the default record mode.

--- ros-pkg/rgbd_sequence/src/record.cpp
+++ ros-pkg/rgbd_sequence/src/record.cpp
@@ -6,26 +6,28 @@ using namespace rgbd;
 string usageString()
 {
   ostringstream oss;
-  oss << "Usage: record [MODE]" << endl;
+  oss << "Usage: record MODE" << endl;
   oss << "  where MODE is --vga (i.e. 640x480) or --qqvga (i.e. 160x120)." << endl;
-  oss << "  The default is --qqvga." << endl;
   return oss.str();
 }
 
 int main(int argc, char** argv)
 {
-  pcl::OpenNIGrabber::Mode mode = pcl::OpenNIGrabber::OpenNI_QQVGA_30Hz;
-  if(argc == 2) {
-    if(strcmp("--vga", argv[1]) == 0)
-      mode = pcl::OpenNIGrabber::OpenNI_VGA_30Hz;
-    else if(strcmp("--qqvga", argv[1]) == 0)
-      mode = pcl::OpenNIGrabber::OpenNI_QQVGA_30Hz;
-    else { 
-      cout << usageString() << endl;
-      return 0;
-    }
+  if(argc != 2) {
+    cout << usageString() << endl;
+    return 0;
   }
-
+  
+  pcl::OpenNIGrabber::Mode mode = pcl::OpenNIGrabber::OpenNI_QQVGA_30Hz;
+  if(strcmp("--vga", argv[1]) == 0)
+    mode = pcl::OpenNIGrabber::OpenNI_VGA_30Hz;
+  else if(strcmp("--qqvga", argv[1]) == 0)
+    mode = pcl::OpenNIGrabber::OpenNI_QQVGA_30Hz;
+  else {
+    cout << usageString() << endl;
+    return 0;
+  }
+  
   Recorder rec("", mode);
   rec.run();
   
