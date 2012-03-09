7648e2f2693c269a047a68a927b59716a438625a
Alex Teichman <teichman@stanford.edu>
2012-03-09 04:57:54 -0800

    Adding sync to plot.

--- project/calibration/plot_bargraph.py
+++ project/calibration/plot_bargraph.py
@@ -21,16 +21,21 @@ os.system("grep Angle evaluations/configuration*.txt | awk '{print $NF}' > " + t
 angles = np.loadtxt(tmpfile)
 os.system("grep Euclidean evaluations/configuration*.txt | awk '{print $NF}' > " + tmpfile)
 translations = np.loadtxt(tmpfile);
+os.system("grep 'Sync error' evaluations/configuration*.txt | awk '{print $NF}' > " + tmpfile)
+syncs = np.loadtxt(tmpfile);
 
 # -- Get overall performances
 average_angle = np.sum(angles) / len(angles)
 average_translation = np.sum(translations) / len(translations)
+average_sync = np.sum(syncs) / len(syncs)
 
 angles = hstack([angles, 0, 0, average_angle, 0])
 translations = hstack([translations, 0, 0, average_translation, 0])
+syncs = hstack([syncs, 0, 0, average_sync, 0])
 
 print angles
 print translations
+print syncs
 
 # -- Set up figure
 indices = np.arange(len(angles))
@@ -38,7 +43,7 @@ width = 0.35
 fig = figure(figsize=(15, 5))
 
 # -- Angles
-ax1 = plt.subplot2grid((2, 5), (0, 1), colspan=4)
+ax1 = plt.subplot2grid((3, 5), (0, 1), colspan=4)
 for loc, spine in ax1.spines.iteritems():
     if loc in ['right','top']:
         spine.set_color('none')
@@ -48,21 +53,33 @@ angle_rects = ax1.bar(indices, angles, width, color='g')
 ax1.yaxis.set_ticks([ax1.yaxis.get_majorticklocs()[0], ax1.yaxis.get_majorticklocs()[-1]])
 
 # -- Translations
-ax2 = plt.subplot2grid((2, 5), (1, 1), colspan=4)
-ax2.xaxis.set_ticks_position('bottom')
-ax2.xaxis.set_ticklabels([str(x) for x in range(1, len(indices) - 3)] + ['', '', 'Overall', ''])
-ax2.xaxis.set_ticks(indices + width)
+ax2 = plt.subplot2grid((3, 5), (1, 1), colspan=4)
 for loc, spine in ax2.spines.iteritems():
     if loc in ['right','top']:
         spine.set_color('none')
-translation_rects = ax2.bar(indices, translations, width, color='g')
 ax2.yaxis.set_ticks_position('left')
+ax2.xaxis.set_ticks([])
+translation_rects = ax2.bar(indices, translations, width, color='g')
 ax2.yaxis.set_ticks([ax2.yaxis.get_majorticklocs()[0], ax2.yaxis.get_majorticklocs()[-1]])
+
+# -- Syncs
+ax3 = plt.subplot2grid((3, 5), (2, 1), colspan=4)
+ax3.xaxis.set_ticks_position('bottom')
+ax3.xaxis.set_ticklabels([str(x) for x in range(1, len(indices) - 3)] + ['', '', 'Overall', ''])
+#ax3.xaxis.set_ticks(indices + width)
+ax3.xaxis.set_ticks(indices + width/2.0)
+for loc, spine in ax3.spines.iteritems():
+    if loc in ['right','top']:
+        spine.set_color('none')
+translation_rects = ax3.bar(indices, syncs, width, color='g')
+ax3.yaxis.set_ticks_position('left')
+ax3.yaxis.set_ticks([ax3.yaxis.get_majorticklocs()[0], ax3.yaxis.get_majorticklocs()[-1]])
 
 # -- Annotate and save
 xlabel('Sequence Number')
-txt1 = figtext(0.1, 0.7, "Angle error\n(radians)", size='xx-large')
-txt2 = figtext(0.1, 0.2, "Translation error\n(m)", size='xx-large')
+txt1 = figtext(0.1, 0.75, "Angle error\n(radians)", size='large')
+txt2 = figtext(0.1, 0.5, "Translation error\n(m)", size='large')
+txt3 = figtext(0.1, 0.25, "Sync error\n(s)", size='large')
 #fl = figlegend(angle_rects, translation_rects), ('Angle error (radians)', 'Translation error (m)'), loc=(0.0, 0.45))
 
 pad = 0.1
--- ros-pkg/xpl_calibration/src/evaluate.cpp
+++ ros-pkg/xpl_calibration/src/evaluate.cpp
@@ -8,16 +8,24 @@ using namespace Eigen;
 string usageString()
 {
   ostringstream oss;
-  oss << "Usage: evaluate GROUND_TRUTH_EIG ESTIMATED_EIG" << endl;
+  oss << "Usage: evaluate GROUND_TRUTH_TRANS ESTIMATED_TRANS GROUND_TRUTH_SYNC ESTIMATED_SYNC" << endl;
   return oss.str();
 }
 
 int main(int argc, char** argv)
 {
-  if(argc != 3) {
+  if(argc != 5) {
     cout << usageString() << endl;
     return 0;
   }
+
+  VectorXd gtt;
+  VectorXd estt;
+  eigen_extensions::loadASCII(argv[3], &gtt);
+  eigen_extensions::loadASCII(argv[4], &estt);
+  cout << "Ground truth sync: " << gtt(0) << endl;
+  cout << "Estimated sync: " << estt(0) << endl;
+  cout << "Sync error: " << fabs(gtt(0) - estt(0)) << endl;
 
   MatrixXd gt;
   MatrixXd est;
@@ -38,8 +46,6 @@ int main(int argc, char** argv)
   AngleAxisd aa(Rest.inverse() * Rgt);
   cout << "Angle: " << aa.angle() << endl;
   cout << "Axis: " << aa.axis().transpose() << endl;
-
-
   
   AngleAxisd final(AngleAxisd(M_PI/2.0, Vector3d::UnitZ()) * AngleAxisd(M_PI/2.0, Vector3d::UnitY()));
   cout << "jesse: " << final.angle() << endl;
