8c718c871609348ebc18acf48696a5d6921fe540
Alex Teichman <teichman@cs.stanford.edu>
2012-10-10 02:28:04 -0700

    First simulated test of Twiddler.

--- ros-pkg/core/pipeline3/include/pipeline/twiddler.h
+++ ros-pkg/core/pipeline3/include/pipeline/twiddler.h
@@ -2,47 +2,60 @@
 #define TWIDDLER_H
 
 #include <signal.h>
+#include <boost/filesystem.hpp>
+#include <ros/assert.h>
+#include <ros/console.h>
 #include <bag_of_tricks/bag_of_tricks.h>
+#include <pipeline/params.h>
 
 namespace pipeline
 {
   
   class Twiddler
   {
+  public:
     //! User-defined results of evaluating a Params set.
     typedef Dictionary<std::string, double> Results;
 
+    // std::vector<Params> params_;
+    // std::vector<Results> results_;
     std::map<Params, Results> results_;
     
     Twiddler();
-    virtual ~Twiddle() {}
+    virtual ~Twiddler() {}
     //! path must not exist and will be created.
-    void initialize(std::string path);
-    //! path must exist.  Loads existing results.
-    void load(std::string path);
     //! Fills results_ and saves to path_ as new evaluations are made.
-    void run(const Params& init);
-    //! results_ must be non-empty.  Picks up where it left off.
-    void resume(); 
+    void run(std::string path, const Params& init);
+    //! path must exist.  Loads existing results.
+    //! results_ must be non-empty.  Finds the best set of results and picks up where it left off.
+    void resume(std::string path); 
 
+    
     /************************************************************
      * Functions to be implemented
      ************************************************************/
 
-    //! By default the returned Results should contain an "objective" field.
-    //! By overloading this method you can interpret additional results.
-    //! For example, you might include "time" in addition to "objective", then overload isImprovement to
-    //! optimize the objective as long as timing constraints are met.
-    //! path is the directory to which you can save things if you want.
-    virtual Results evaluate(const Params& params, std::string path) = 0;
+    virtual Results evaluate(std::string path, const Params& params) = 0;
     //! Given the best current params, generate a new set of params.
     //! You can implement whatever search strategy you want here.
     //! Twiddler will check whether your new Params is a duplicate.
-    virtual Params generateParamVariation(const Params& params) const = 0;
+    virtual Params generateParamVariation(Params params) const = 0;
     //! By default, this returns the field called "objective".
     //! You can overload this to handle constraints as well as different objectives.
     //! Lower is better.
     virtual double objective(const Results& results) const;
+    //! This is called any time an improved set of params is found.
+    virtual void improvementHook(const Params& params, const Results& results) const;
+    //! Termination criteria.  Never terminates by default.
+    virtual bool done(const Results& results) const;
+
+    
+    /************************************************************
+     * Miscellaneous things.
+     ************************************************************/
+
+    void getBest(Params* best_params, Results* best_results) const;
+
     
     /************************************************************
      * Useful functions for generating parameter variations
@@ -53,6 +66,8 @@ namespace pipeline
   protected:
     std::string path_;
     size_t next_id_;
+
+    void twiddle();
   };
 
 }
