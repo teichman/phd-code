38b7445ac5254fb8fc23f3a219ba05fad9083299
Alex Teichman <teichman@cs.stanford.edu>
2012-10-17 15:07:21 -0700

    Adding example custom callback, simple test program for dlvis.

--- ros-pkg/core/dlvis/src/prog/dlvis_test.cpp
+++ ros-pkg/core/dlvis/src/prog/dlvis_test.cpp
@@ -1,22 +1,48 @@
-#include <dlvis/dlvis.h>
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
 #include <pcl/io/pcd_io.h>
+#include <xpl_calibration/object_matching_calibrator.h>
+#include <dlvis/dlvis.h>
 
 using namespace std;
+using namespace Eigen;
 using namespace rgbd;
+
+void keyboardCallback(unsigned char c, int x, int y)
+{
+  cout << "Got keypress: " << c << endl;
+}
 
 int main(int argc, char** argv)
 {
-  DLVis vis;
   Cloud::Ptr all(new Cloud);
-  for(int i = 1; i < argc; ++i) {
+  bool ps = false;
+  int i = 1;
+  if(string(argv[1]) == "--primesense") {
+    ps = true;
+    ++i;
+  }
+  for(; i < argc; ++i) {
     cout << "Loading " << argv[i] << endl;
     Cloud pcd;
     pcl::io::loadPCDFile<Point>(argv[i], pcd);
+    if(ps) {
+      Affine3f transform = generateTransform(- M_PI / 2.0, 0.0, -M_PI / 2.0, 0, 0, 0);
+      pcl::transformPointCloud(pcd, pcd, transform);
+    }
     *all += pcd;
   }
 
+  DLVis vis;
+  vis.registerKeyboardCallback(keyboardCallback);
   ThreadPtr thread = vis.launch();
   vis.setPointCloud(all);
+
+  while(true) {
+    usleep(1e6);
+    // do something else, update visualizer, etc.
+  }
+  
   thread->join();
   return 0;
 }
