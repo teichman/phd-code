bb42f62f9e2b3663c847fbb49f5977e29ba32fcf
Alex Teichman <teichman@cs.stanford.edu>
2012-10-28 22:24:13 -0700

    Adding overhead term to timing plot for 160x120.  Placeholder for 640x480.

--- project/dst/plot_timing.py
+++ project/dst/plot_timing.py
@@ -31,6 +31,8 @@ os.system("grep 'Total feature computation' `find " + avspath + " -name testing_
 qqvga_feature = np.loadtxt(tmpfile)
 os.system("grep maxflow `find " + avspath + " -name testing_results.txt-log.txt | grep mask1_skip02` | awk '{sum += $2} END {print sum / NR}' > " + tmpfile)
 qqvga_maxflow = np.loadtxt(tmpfile)
+os.system("grep 'Average time' `find " + avspath + " -name testing_results.txt | grep mask1_skip02` | awk '{sum += $NF} END {print sum / NR}' > " + tmpfile)
+qqvga_total = np.loadtxt(tmpfile)
 
 os.system("rm " + tmpfile)
 
@@ -39,28 +41,28 @@ figw = 15
 figh = 5
 fig = plt.figure(figsize = (figw, figh))
 
-def timingPlot(ax, feat, gc):
-    times = np.array([feat, gc])
+def timingPlot(ax, feat, gc, total):
+    times = np.array([feat, gc, total - feat - gc])
     fracs = times / (feat + gc);
     labels = [("%0.0f ms" % x) for x in times]
-    explode = [0.05, 0.05]
+    explode = [0.05, 0.05, 0.05]
     print fracs
-    pie = ax.pie(fracs, explode=explode, labels=labels, autopct='', shadow=False)
+    pie = ax.pie(fracs, explode=explode, labels=labels, autopct='', shadow=False, colors=('g', 'r', 'b'))
     for w in pie[0]:
         w.set_alpha(0.5)
     return pie
 
 vga_ax = fig.add_subplot(1, 3, 1, aspect='equal')
-timingPlot(vga_ax, vga_feature, vga_maxflow)
+timingPlot(vga_ax, vga_feature, vga_maxflow, vga_feature + vga_maxflow)
 vga_ax.set_title("640 $\\times$ 480", bbox={'facecolor':'0.8', 'pad':10}, position=(0.5, -0.1))
 
 # -- Plot QQVGA results.
 qqvga_ax = fig.add_subplot(1, 3, 3, aspect='equal')
-pie = timingPlot(qqvga_ax, qqvga_feature, qqvga_maxflow)
+pie = timingPlot(qqvga_ax, qqvga_feature, qqvga_maxflow, qqvga_total)
 qqvga_ax.set_title("160 $\\times$ 120", bbox={'facecolor':'0.8', 'pad':10}, position=(0.5, -0.1))
 
 # -- Legend.
-leg = fig.legend(pie[0], ("Features", "Graph cuts"), loc='center', bbox_to_anchor = (0.5, 0.5))
+leg = fig.legend(pie[0], ("Features", "Graph cuts", "Other"), loc='center', bbox_to_anchor = (0.5, 0.5))
 
 # -- Save.
 savefig(avspath + '/timing.png')
