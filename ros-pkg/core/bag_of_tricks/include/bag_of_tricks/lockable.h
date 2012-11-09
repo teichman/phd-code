ac1a339eaf190533c61612ffbbe53c1085dd4cbd
Alex Teichman <teichman@cs.stanford.edu>
2012-11-09 14:56:07 -0800

    Making SharedLockable objects assignable and copy-constructable by ignoring the mutex.

--- ros-pkg/core/bag_of_tricks/include/bag_of_tricks/lockable.h
+++ ros-pkg/core/bag_of_tricks/include/bag_of_tricks/lockable.h
@@ -9,10 +9,20 @@
 #define scopeLockRead boost::shared_lock<boost::shared_mutex> lockable_shared_lock(shared_mutex_)
 #define scopeLockWrite boost::unique_lock<boost::shared_mutex> lockable_unique_lock(shared_mutex_)
 
+/* SharedLockable is based on the uncopyable boost::shared_mutex.
+   This presents a dilemma when assigning or copy constructing.
+   Right now, the state of the mutex in the other SharedLockable
+   does not get copied to the target SharedLockable.
+   I'm not sure yet if this is the desired behavior.
+*/
 class SharedLockable
 {
 public:
   SharedLockable();
+  //! Copy constructor will make a new shared_mutex that is unlocked.
+  SharedLockable(const SharedLockable& other) {}
+  //! Assignment operator will *not* copy the shared_mutex_ or the state of shared_mutex_ from other.
+  SharedLockable& operator=(const SharedLockable& other) { return *this; }
 
   void lockWrite();
   void unlockWrite();
@@ -23,6 +33,8 @@ public:
   bool trylockRead();
 
 protected:
+  //! For the first time ever, I'm tempted to make this mutable.
+  //! It'd make user methods still be able to be const even if they are locking.
   boost::shared_mutex shared_mutex_;
 };
 
