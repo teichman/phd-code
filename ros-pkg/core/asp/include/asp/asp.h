28b0aed3f01495da996ecbcab7afd316f994d396
Alex Teichman <teichman@cs.stanford.edu>
2012-12-19 02:46:40 -0800

    In the middle of working on asp.  Definitely won't compile.

--- ros-pkg/core/asp/include/asp/asp.h
+++ ros-pkg/core/asp/include/asp/asp.h
@@ -1,36 +1,134 @@
 #ifndef ASP_H
 #define ASP_H
 
-//#include <pipeline/pipeline.h>
+#include <pipeline/pipeline.h>
 #include <graphcuts/potentials_cache.h>
 #include <name_mapping/name_mapping.h>
 
-// This is bad.  Don't ever do this.
-namespace gc = graphcuts;
-//namespace pl = pipeline;
+namespace asp {
+  using namespace gc;
+  using namespace pl;
 
-class AbstractSegmentationPipeline
-{
-public:
-  //pl::Pipeline pipeline_;
-
-  //! Initializes pipeline_ with NodePotentialAggregator, EdgePotentialAggregator, 
-  //! and GraphCuts pods.  You then hook up your own things to this.
-  AbstractSegmentationPipeline();
-
-  void setWeights(const ASPWeights& weights);
-  //! Names are automatically filled in based on Pipeline pod names.
-  ASPWeights getWeights() const;
-  //! Runs structural SVM solver.  Does not set weights.
-  ASPWeights train(const std::vector<gc::PotentialsCache::ConstPtr>& caches,
-		   const std::vector<gc::VecXiConstPtr>& labels) const;
-  //! You have to set the input to the pipeline yourself.
-  //! segmentation will be resized if it is not the correct length.
-  void segment(Eigen::VectorXi* segmentation);
-  //! Potentials from the last run of segment().  You generally only run
-  //! this when building a training set.
-  void getPotentialsCache(gc::PotentialsCache* pc) const;
+  typedef boost::shared_ptr<Eigen::MatrixXf> MatrixXfPtr;
+  typedef boost::shared_ptr<const Eigen::MatrixXf> MatrixXfConstPtr;
   
-};
+  //! Abstract Segmentation Pipeline.
+  class Asp
+  {
+  public:
+    Pipeline pipeline_;
+
+    Asp();
+    virtual ~Asp() {};
+
+    //! Names are automatically filled in based on Pipeline pod names.
+    //! Weights need not be set in NPA and EPA.
+    Model defaultModel() const;
+    //! Names are automatically filled in based on Pipeline pod names.
+    //! Weights must be set in NPA and EPA.
+    Model model() const;
+    //! Installs weights into NPA and EPA.
+    //! Names in nmap and emap must match node potential and edge potential pod names.
+    void setModel(const Model& model);
+
+    //! You have to set the input to the pipeline yourself first.
+    //! Also, setModel() must have been called.
+    //! segmentation will be resized if it is not the correct length.
+    //! cache will be filled if it is not null.
+    void segment(Eigen::VectorXi* segmentation, PotentialsCache* cache = NULL);
+
+  protected:
+    //! Initializes pipeline_ with NodePotentialAggregator, EdgePotentialAggregator, 
+    //! and GraphCuts pods.  You then hook up your own things to this.
+    void initializePipeline();
+  };
+
+  class NodePotentialGenerator : public pl::Pod
+  {
+  public:
+    NodePotentialGenerator(std::string name) :
+      Pod(name),
+      source_(new MatrixXf),
+      sink_(new MatrixXf)
+    {
+      declareInput<cv::Mat3b>("Image");
+      declareOutput<MatrixXfConstPtr>("SourcePotentials");
+      declareOutput<MatrixXfConstPtr>("SinkPotentials");
+    }
+
+  protected:
+    MatrixXfConstPtr source_;
+    MatrixXfConstPtr sink_;
+
+    //void displayNodePotentials(const cv::Mat3b background = cv::Mat3b()) const;
+    void displayNodePotentials() const;
+  };
+  
+  class NodePotentialAggregator : public NodePotentialGenerator
+  {
+  public:
+    DECLARE_POD(NodePotentialAggregator);
+    NodePotentialAggregator(std::string name) :
+      NodePotentialGenerator(name)
+    {
+      declareInput<MatrixXfConstPtr>("UnweightedSourcePotentials");
+      declareInput<MatrixXfConstPtr>("UnweightedSinkPotentials");
+    }
+
+    //! Checks that names match.
+    void setWeights(const Model& model);
+    //! Sets nmap names based on input pod names.
+    //! Sets model->nweights_ to local nweights_.
+    void fillModel(Model* model) const;
+    
+  protected:
+    MatrixXfPtr aggregated_;
+    Eigen::VectorXf nweights_;
+
+    void compute();
+    void debug() const;
+  };
+  
+  class EdgePotentialGenerator : public pl::Pod
+  {
+  public:
+    EdgePotentialGenerator(std::string name) :
+      Pod(name),
+      edge_(new DynamicSparseMat)
+    {
+      declareInput<cv::Mat3b>("Image");
+      declareOutput<DynamicSparseMatConstPtr>("EdgePotentials");
+    }
+
+    DynamicSparseMatPtr edge_;
+
+    //! Allocates new sparse matrix if necessary; just sets to zero otherwise.
+    void initializeStorage(int num_nodes, double reserve_per_node = 2);
+    //void displayEdges(cv::Mat3b img) const;
+    void displayEdgePotentials() const;
+  };
+
+  class EdgePotentialAggregator : public EdgePotentialGenerator
+  {
+  public:
+
+  protected:
+    void debug() const;
+  };
+
+  class GraphcutsPod : public pl::Pod
+  {
+  public:
+    DECLARE_POD(GraphcutsPod);
+    GraphcutsPod(std::string name) :
+      Pod(name)
+    {
+      declareInput<MatrixXfConstPtr>("AggregatedNodePotentials");
+      declareInput<DynamicSparseMatConstPtr>("AggregatedEdgePotentials");
+      
+    }
+  };
+  
+}
 
 #endif // ASP_H
--- ros-pkg/core/asp/src/lib/asp.cpp
+++ ros-pkg/core/asp/src/lib/asp.cpp
@@ -2,43 +2,34 @@
 
 using namespace std;
 
-void ASPWeights::serialize(std::ostream& out) const
+void NodePotentialAggregator::setWeights(const Model& model)
 {
-  serializeNameMappings(out);
-  eigen_extensions::serialize(nweights_, out);
-  eigen_extensions::serialize(eweights_, out);
+  const vector<const Outlet*>& inputs = inputs_["NodePotentials"];
+  ROS_ASSERT((size_t)model.nweights_.rows() == inputs.size());
+  ROS_ASSERT((size_t)model.nweights_.rows() == model.nameMapping("nmap").size());
+  for(size_t i = 0; i < inputs.size(); ++i)
+    ROS_ASSERT(inputs[i]->getPod()->getName() == model.nameMapping("nmap").toName(i));
+  
+  nweights_ = model.nweights_;
 }
 
-void ASPWeights::deserialize(std::istream& in)
+void NodePotentialAggregator::fillModel(Model* model) const
 {
-  deserializeNameMappings(in);
-  eigen_extensions::deserialize(in, &nweights_);
-  eigen_extensions::deserialize(in, &eweights_);
+  const vector<const Outlet*>& inputs = inputs_["NodePotentials"];
+  ROS_ASSERT((size_t)nweights_.rows() == inputs.size());
+  NameMapping nmap;
+  for(size_t i = 0; i < inputs.size(); ++i) {
+    nmap.addName(inputs[i]->getPod()->getName());
+  }
+  model->applyNameMapping("nmap", nmap);
+  model->nweights_ = nweights_;
 }
 
-void ASPWeights::_applyNameTranslator(const std::string& id, const NameTranslator& translator)
+void NodePotentialAggregator::compute()
 {
-  ROS_ASSERT(id == "nmap" || id == "emap");
-  if(id == "nmap")
-    translator.translate(&nweights_, 0.0);
-  else if(id == "emap")
-    translator.translate(&eweights_, 0.0);
-}
+  vector<MatrixXfConstPtr> npots;
+  pull("NodePotentials", &npots);
 
-std::string ASPWeights::status(const std::string& prefix) const
-{
-  ostringstream oss;
-  ROS_ASSERT((size_t)nweights_.rows() == nameMapping("nmap").size());
-  ROS_ASSERT((size_t)eweights_.rows() == nameMapping("emap").size());
-
-  oss << prefix << "Node potentials" << endl;
-  for(size_t i = 0; i < nameMapping("nmap").size(); ++i) 
-    oss << prefix << "  " << setiosflags(ios::left) << setw(25) << nameMapping("nmap").toName(i) << ": " << nweights_[i] << endl;
-
-  oss << prefix << "Edge potentials" << endl;
-  for(size_t i = 0; i < nameMapping("emap").size(); ++i) 
-    oss << prefix << "  " << setiosflags(ios::left) << setw(25) << nameMapping("emap").toName(i) << ": " << eweights_[i] << endl;
-
-  return oss.str();
+  ROS_ASSERT((size_t)nweights_.rows() == npots.size());
 }
 
