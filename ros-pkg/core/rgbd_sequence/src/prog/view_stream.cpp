96868e413c4834976e70549b9db6be05b4a9f80e
Alex Teichman <teichman@cs.stanford.edu>
2013-01-21 16:08:58 -0800

    Adding first version of the planarity experiment and a better version of view_stream which lets you measure distances to the camera.

--- ros-pkg/core/rgbd_sequence/src/prog/view_stream.cpp
+++ ros-pkg/core/rgbd_sequence/src/prog/view_stream.cpp
@@ -1,11 +1,116 @@
 #include <boost/program_options.hpp>
-#include <rgbd_sequence/stream_sequence.h>
 #include <pcl/visualization/cloud_viewer.h>
 #include <opencv2/highgui/highgui.hpp>
+#include <rgbd_sequence/stream_sequence.h>
+#include <bag_of_tricks/agent.h>
 
 using namespace std;
 using namespace rgbd;
+using namespace pcl::visualization;
 namespace bpo = boost::program_options;
+
+class StreamVisualizer : public Agent
+{
+public:
+  StreamVisualizer(StreamSequence::ConstPtr sseq);
+  void _run();
+
+protected:
+  PCLVisualizer vis_;
+  StreamSequence::ConstPtr sseq_;
+  int idx_;
+  Cloud::Ptr pcd_;
+  
+  void pointPickingCallback(const pcl::visualization::PointPickingEvent& event, void* cookie);
+  void keyboardCallback(const pcl::visualization::KeyboardEvent& event, void* cookie);
+  void increment(int num);
+};
+
+StreamVisualizer::StreamVisualizer(StreamSequence::ConstPtr sseq) :
+  sseq_(sseq),
+  idx_(0),
+  pcd_(new Cloud)
+{
+  vis_.addCoordinateSystem(0.25);
+  vis_.setBackgroundColor(255, 255, 255);
+  vis_.registerPointPickingCallback(&StreamVisualizer::pointPickingCallback, *this);
+  vis_.registerKeyboardCallback(&StreamVisualizer::keyboardCallback, *this);
+
+  increment(0);
+}
+
+void StreamVisualizer::_run()
+{
+  while(true) {
+    if(pcd_->empty()) {
+      Point pt;
+      pt.x = 0;
+      pt.y = 0;
+      pt.z = 0;
+      pcd_->push_back(pt);
+    }
+    if(!vis_.updatePointCloud(pcd_, "default"))
+      vis_.addPointCloud(pcd_, "default");
+
+    vis_.spinOnce();
+
+    scopeLockWrite;
+    if(quitting_)
+      break;
+  }
+}
+
+void StreamVisualizer::keyboardCallback(const pcl::visualization::KeyboardEvent& event, void* cookie)
+{
+  if(event.keyDown()) {
+    char key = event.getKeyCode();
+    if(key == 27) {
+      scopeLockWrite;
+      quitting_ = true;
+    }
+    else if(key == '>')
+      increment(10);
+    else if(key == '<')
+      increment(-10);
+    else if(key == '.')
+      increment(1);
+    else if(key == ',')
+      increment(-1);
+  }
+}
+
+void StreamVisualizer::pointPickingCallback(const pcl::visualization::PointPickingEvent& event, void* cookie)
+{
+  if(event.getPointIndex() == -1)
+    return;
+    
+  Point pt;
+  event.getPoint(pt.x, pt.y, pt.z);
+  cout << "Selected point: " << pt.x << ", " << pt.y << ", " << pt.z << endl;
+  vis_.removeAllShapes();
+  
+  Point origin;
+  origin.x = 0;
+  origin.y = 0;
+  origin.z = 0;
+  vis_.addArrow<Point, Point>(origin, pt, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, "line");
+}
+
+void StreamVisualizer::increment(int num)
+{
+  scopeLockWrite;
+
+  vis_.removeAllShapes();
+  
+  idx_ += num;
+  idx_ = max(0, idx_);
+  idx_ = min((int)sseq_->size(), idx_);
+
+  Frame frame;
+  sseq_->readFrame(idx_, &frame);
+  sseq_->model_.frameToCloud(frame, pcd_.get());
+}
+
 
 int main(int argc, char** argv)
 {
@@ -30,49 +135,66 @@ int main(int argc, char** argv)
   bpo::notify(opts);
   
   cout << "Looking at dir: " << dir << endl;
-  StreamSequence seq;
-  seq.load(dir);
+  StreamSequence::Ptr sseq(new StreamSequence);
+  sseq->load(dir);
   cout << "Loaded successfully" << endl;
 
   double mean_dt = 0;
   double max_dt = -std::numeric_limits<double>::max();
-  for(size_t i = 1; i < seq.size(); i++) {
-    double dt = seq.timestamps_[i] - seq.timestamps_[i-1];
+  for(size_t i = 1; i < sseq->size(); i++) {
+    double dt = sseq->timestamps_[i] - sseq->timestamps_[i-1];
     mean_dt += dt;
     if(dt > max_dt)
       max_dt = dt;
   }
-  mean_dt /= (double)seq.size();
+  mean_dt /= (double)sseq->size();
   cout << "--------------------" << endl;
-  cout << "Mean fps: " << (double)seq.size() / (seq.timestamps_.back() - seq.timestamps_.front()) << endl;
+  cout << "Mean fps: " << (double)sseq->size() / (sseq->timestamps_.back() - sseq->timestamps_.front()) << endl;
   cout << "Mean dt: " << mean_dt << endl;
   cout << "Max dt: " << max_dt << endl;
   cout << "--------------------" << endl;
   cout << "Sensor model: " << endl;
-  cout << seq.model_.status("  ");
+  cout << sseq->model_.status("  ");
   
   if(opts.count("only-stats"))
     return 0;
+
+
+  StreamVisualizer vis(sseq);
+  vis.run();
+  return 0;
+
   
-  pcl::visualization::CloudViewer cloud_viewer("cloud");
-  cv::namedWindow("image");
-  Frame frame;
-  for(size_t i = 0; i < seq.size(); i++){
-    cout << "Viewing cloud: " << i << endl;
-    seq.readFrame(i, &frame);
+  // pcl::visualization::PCLVisualizer vis("cloud");
+  // //pcl::visualization::CloudViewer cloud_viewer("cloud");
+  // //vis.registerPointPickingCallback(pointPickingCallback);
+  // vis.registerKeyboardCallback(keyboardCallback);
+  // cv::namedWindow("image");
+  // Frame frame;
+  // size_t idx = 0;
+  // while(idx < seq.size()) {
 
-    Cloud::Ptr cloud(new Cloud);
-    seq.model_.frameToCloud(frame, cloud.get());
-    cloud_viewer.showCloud(cloud);
 
-    cv::imshow("image", frame.img_);
+  //   cout << "Viewing cloud: " << idx << endl;
+  //   seq.readFrame(idx, &frame);
 
-    if(i < seq.size()) {
-      double dt = seq.timestamps_[i+1]-seq.timestamps_[i];
-      cout << "dt: " << dt << endl;
-      cv::waitKey(dt * 1e3);
-    }
-  }
+  // }
+  // for(size_t i = 0; i < seq.size(); i++){
+  //   cout << "Viewing cloud: " << i << endl;
+  //   seq.readFrame(i, &frame);
+
+  //   Cloud::Ptr cloud(new Cloud);
+  //   seq.model_.frameToCloud(frame, cloud.get());
+  //   vis.showCloud(cloud);
+
+  //   cv::imshow("image", frame.img_);
+
+  //   if(i < seq.size()) {
+  //     double dt = seq.timestamps_[i+1]-seq.timestamps_[i];
+  //     cout << "dt: " << dt << endl;
+  //     cv::waitKey(dt * 1e3);
+  //   }
+  // }
   
   return 0;
 }
---
+++ ros-pkg/core/xpl_calibration/src/prog/planarity_experiment.cpp
@@ -0,0 +1,119 @@
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+#include <rgbd_sequence/stream_sequence.h>
+
+using namespace std;
+using namespace Eigen;
+using namespace rgbd;
+
+bool process(const StreamSequence& sseq, size_t idx, double* mean_range, double* rms)
+{
+  Frame frame;
+  sseq.readFrame(idx, &frame);
+  Cloud cloud;
+  sseq.model_.frameToCloud(frame, &cloud);
+
+  double total = 0;
+  for(size_t i = 0; i < cloud.size(); ++i)
+    if(isFinite(cloud[i]))
+      ++total;
+
+  if(total < 30000)
+    return false;
+
+  Vector3f mean = Vector3f::Zero();
+  for(size_t i = 0; i < cloud.size(); ++i)
+    if(isFinite(cloud[i]))
+      mean += cloud[i].getVector3fMap();
+  mean /= total;
+  
+  Matrix3d X = Matrix3d::Zero();
+  for(size_t i = 0; i < cloud.size(); ++i)
+    if(isFinite(cloud[i]))
+      X += ((cloud[i].getVector3fMap() - mean) * (cloud[i].getVector3fMap() - mean).transpose()).cast<double>();
+
+  //cout << X << endl;
+  //cout << endl;
+
+  JacobiSVD<Matrix3d> svd(X, Eigen::ComputeFullU | Eigen::ComputeFullV);
+  Matrix3d U = svd.matrixU();
+  Matrix3d V = svd.matrixV();
+  Vector3d svals = svd.singularValues();
+
+  //cout << U << endl;
+  //cout << endl;
+  //cout << svals.transpose() << endl;
+  //cout << endl;
+  //cout << V << endl;
+  //cout << endl;
+
+  Vector3f normal = V.col(2).cast<float>();
+  //cout << "Normal: " << normal.transpose() << endl;
+  
+  double mean_offset = 0;
+  for(size_t i = 0; i < cloud.size(); ++i)
+    if(isFinite(cloud[i]))
+      mean_offset += normal.dot(cloud[i].getVector3fMap());
+  mean_offset /= total;
+  //cout << "mean_offset: " << mean_offset << endl;
+
+  *mean_range = 0;
+  for(size_t i = 0; i < cloud.size(); ++i)
+    if(isFinite(cloud[i]))
+      *mean_range += cloud[i].z;
+  *mean_range /= total;
+  //cout << "Mean range: " << *mean_range << endl;
+  
+  *rms = 0;
+  for(size_t i = 0; i < cloud.size(); ++i)
+    if(isFinite(cloud[i]))
+      *rms += pow(normal.dot(cloud[i].getVector3fMap()) - mean_offset, 2);
+  *rms /= total;
+  *rms = sqrt(*rms);
+  //cout << "RMS: " << *rms << endl;
+
+  return true;
+}
+
+int main(int argc, char** argv)
+{
+  namespace bpo = boost::program_options;
+  namespace bfs = boost::filesystem;
+  bpo::options_description opts_desc("Allowed options");
+  bpo::positional_options_description p;
+
+  opts_desc.add_options()
+    ("help,h", "produce help message")
+    ("sseq", bpo::value<string>()->required(), "StreamSequence, i.e. asus data.")
+//    ("output,o", bpo::value<string>()->default_value("planarity_experiment.txt"), "Where to save results")
+    ;
+
+  p.add("sseq", 1);
+  
+  bpo::variables_map opts;
+  bool badargs = false;
+  try {
+    bpo::store(bpo::command_line_parser(argc, argv).options(opts_desc).positional(p).run(), opts);
+    bpo::notify(opts);
+  }
+  catch(...) { badargs = true; }
+  if(opts.count("help") || badargs) {
+    cout << "Usage: " << bfs::basename(argv[0]) << " SSEQ" << endl;
+    cout << endl;
+    cout << opts_desc << endl;
+    return 1;
+  }
+
+  StreamSequence sseq;
+  sseq.load(opts["sseq"].as<string>());
+
+  for(size_t i = 0; i < sseq.size(); i += 10) {
+    double mean_range;
+    double rms;
+    bool valid = process(sseq, i, &mean_range, &rms);
+    if(valid)
+      cout << mean_range << " " << rms << endl;
+  }
+
+  return 0;
+}
