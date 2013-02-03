600e8e755b6b3d6715a9e447d93dfe39687edab0
Alex Teichman <teichman@cs.stanford.edu>
2013-02-03 00:51:56 -0800

    inspect_depth_image can make use of intrinsics and show pcds.

--- ros-pkg/core/xpl_calibration/src/prog/inspect_depth_image.cpp
+++ ros-pkg/core/xpl_calibration/src/prog/inspect_depth_image.cpp
@@ -1,19 +1,28 @@
+#include <boost/program_options.hpp>
 #include <pcl/io/openni_grabber.h>
+#include <pcl/visualization/cloud_viewer.h>
 #include <image_labeler/opencv_view.h>
 #include <bag_of_tricks/lockable.h>
+#include <xpl_calibration/discrete_depth_distortion_model.h>
 
 using namespace std;
+using namespace Eigen;
+using namespace rgbd;
 
 class Inspector : public OpenCVViewDelegate, public Lockable
 {
 public:
+  DiscreteDepthDistortionModel* dddm_;
+  bool show_pcd_;
+  
   void run();
   Inspector();
   
 protected:
-  cv::Mat1d dmap_;
+  Frame frame_;
   cv::Mat3b vis_;
   OpenCVView view_;
+  bool use_intrinsics_;
 
   void mouseEvent(int event, int x, int y, int flags, void* param);
   void callback(const boost::shared_ptr<openni_wrapper::DepthImage>&);
@@ -22,16 +31,21 @@ protected:
 };
 
 Inspector::Inspector() :
-  view_("Depth image")
+  dddm_(NULL),
+  show_pcd_(false),
+  view_("Depth image"),
+  use_intrinsics_(false)
 {
   view_.setDelegate(this);
+  frame_.depth_ = DepthMatPtr(new DepthMat(480, 640));
+  frame_.depth_->setZero();
 }
 
 void Inspector::mouseEvent(int event, int x, int y, int flags, void* param)
 {
   lock();
   if(event == CV_EVENT_LBUTTONDOWN) {
-    cout << "Click at " << x << " " << y << ".  Depth: " << dmap_(y, x) << endl;
+    cout << "Click at " << x << " " << y << ".  Depth: " << frame_.depth_->coeffRef(y, x) * 0.001 << endl;
   }
   unlock();
 }
@@ -40,7 +54,6 @@ void Inspector::run()
 {
   pcl::OpenNIGrabber::Mode mode = pcl::OpenNIGrabber::OpenNI_VGA_30Hz;
   cv::Size sz(640, 480);
-  dmap_ = cv::Mat1d(sz);
   vis_ = cv::Mat3b(sz, cv::Vec3b(0, 0, 0));
   
   pcl::OpenNIGrabber grabber("", mode, mode);
@@ -67,6 +80,10 @@ void Inspector::run()
       grabber.getDevice()->setDepthRegistration(!grabber.getDevice()->isDepthRegistered());
       cout << "Depth - RGB registration: " << grabber.getDevice()->isDepthRegistered() << endl;
       break;
+    case 'm':
+      use_intrinsics_ = !use_intrinsics_;
+      cout << "use_intrinsics_: " << use_intrinsics_ << endl;
+      break;
     default:
       break;
     }
@@ -75,7 +92,7 @@ void Inspector::run()
 
 void Inspector::updateDepth(const openni_wrapper::DepthImage& oni)
 {
-  dmap_ = 0;
+  frame_.depth_->setZero();
   ushort data[oni.getHeight() * oni.getWidth()];
   oni.fillDepthImageRaw(oni.getWidth(), oni.getHeight(), data);
   int i = 0;
@@ -83,22 +100,43 @@ void Inspector::updateDepth(const openni_wrapper::DepthImage& oni)
     for(size_t x = 0; x < oni.getWidth(); ++x, ++i) {
       if(data[i] == oni.getNoSampleValue() || data[i] == oni.getShadowValue())
 	continue;
-      dmap_(y, x) = data[i] * 0.001f;
+      frame_.depth_->coeffRef(y, x) = data[i];
     }
   }
 
-  vis_ = cv::Vec3b(0, 0, 0);
-  for(int y = 0; y < dmap_.rows; ++y) {
-    for(int x = 0; x < dmap_.cols; ++x) {
-      if(dmap_(y, x) == 0)
-	continue;
-      
-      double maxdist = 10;
-      double mindist = 1;
-      double val = 255 * (1.0 - ((fmin(maxdist, fmax(mindist, dmap_(y, x))) - mindist) / (maxdist - mindist)));
-      vis_(y, x) = cv::Vec3b(val, val, val);
-    }
+  if(dddm_ && use_intrinsics_)
+    dddm_->undistort(&frame_);
+  
+  vis_ = frame_.depthImage();
+  frame_.img_ = vis_.clone();
+
+  static pcl::visualization::CloudViewer viewer("pcd");
+
+  if(show_pcd_) {
+    Cloud::Ptr cloud(new Cloud);
+    PrimeSenseModel model;
+    model.width_ = 640;
+    model.height_ = 480;
+    model.cx_ = model.width_ / 2;
+    model.cy_ = model.height_ / 2;
+    model.fx_ = 525;
+    model.fy_ = 525;
+    model.frameToCloud(frame_, cloud.get());
+    viewer.showCloud(cloud);
   }
+  
+  // vis_ = cv::Vec3b(0, 0, 0);
+  // for(int y = 0; y < frame_.depth_->rows(); ++y) {
+  //   for(int x = 0; x < frame_.depth_->cols(); ++x) {
+  //     if(frame_.depth_->coeffRef(y, x) == 0)
+  // 	continue;
+      
+  //     double maxdist = 10;
+  //     double mindist = 1;
+  //     double val = 255 * (1.0 - ((fmin(maxdist, fmax(mindist, frame_.depth_->coeffRef(y, x))) - mindist) / (maxdist - mindist)));
+  //     vis_(y, x) = cv::Vec3b(val, val, val);
+  //   }
+  // }
 }
 
 void Inspector::callback(const boost::shared_ptr<openni_wrapper::DepthImage>& oni)
@@ -110,7 +148,48 @@ void Inspector::callback(const boost::shared_ptr<openni_wrapper::DepthImage>& on
 
 int main(int argc, char** argv)
 {
+  namespace bpo = boost::program_options;
+  namespace bfs = boost::filesystem;
+  bpo::options_description opts_desc("Allowed options");
+  bpo::positional_options_description p;
+
+  string sequence_path;
+  string trajectory_path;
+  opts_desc.add_options()
+    ("help,h", "produce help message")
+    ("intrinsics", bpo::value<string>(), "Optional discrete distortion model.")
+    ("pcd,p", "Show pointcloud.")
+    ;
+
+  bpo::variables_map opts;
+  bool badargs = false;
+  try {
+    bpo::store(bpo::command_line_parser(argc, argv).options(opts_desc).positional(p).run(), opts);
+    bpo::notify(opts);
+  }
+  catch(...) { badargs = true; }
+  if(opts.count("help") || badargs) {
+    cout << "Usage: " << bfs::basename(argv[0]) << " OPTS" << endl;
+    cout << endl;
+    cout << opts_desc << endl;
+    return 1;
+  }
+
+  int retval = system("killall XnSensorServer"); --retval;
+  
   Inspector inspector;
+  if(opts.count("intrinsics")) {
+    inspector.dddm_ = new DiscreteDepthDistortionModel;
+    inspector.dddm_->load(opts["intrinsics"].as<string>());
+    cout << "Loading DiscreteDepthDistortionModel at " << opts["intrinsics"].as<string>() << endl;
+  }
+  if(opts.count("pcd"))
+    inspector.show_pcd_ = true;
+  
   inspector.run();
+
+  if(inspector.dddm_)
+    delete inspector.dddm_;
+  
   return 0;
 }
