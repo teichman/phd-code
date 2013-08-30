45c79dffdbc6ede9c98ce900b5b2db760fbfcb36
Alex Teichman <teichman@cs.stanford.edu>
2013-08-30 12:37:45 -0700

    Adding optical flow experiment.

---
+++ ros-pkg/core/nutcracker/src/prog/optical_flow_experiment.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <timer/timer.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  cv::VideoCapture cap(argv[1]);
+  if(!cap.isOpened()) {
+    cout << "Failed to open " << argv[1] << endl;
+    return 1;
+  }
+
+  cv::namedWindow("flow", 1);
+
+  cv::Mat1b curr;
+  cv::Mat1b prev;
+  vector<cv::Point2f> curr_pts;
+  vector<cv::Point2f> prev_pts;
+  cv::Mat3b orig;
+  cv::Mat3b curr3b;
+  cv::Mat3b prev3b;
+  cv::Mat3b flow_vis;
+  cv::Mat3b features_vis;
+  cv::Rect roi(390, 219, 1074, 343);
+  cv::Mat1b delta;
+  cv::Mat3b surf_vis;
+  
+  for(int i = 0; i < 100; ++i) cap >> orig;
+
+  cap >> orig;
+  cv::Mat(orig, roi).copyTo(curr3b);
+  cv::cvtColor(curr3b, curr, CV_BGR2GRAY);
+
+  int ms = 0;
+  
+  while(true) {
+    prev = curr.clone();
+    prev3b = curr3b.clone();
+    
+    cap >> orig;
+    cv::Mat(orig, roi).copyTo(curr3b);
+    cv::cvtColor(curr3b, curr, CV_BGR2GRAY);
+        
+    // -- goodFeaturesToTrack.
+    prev_pts.clear();
+    cv::goodFeaturesToTrack(prev, prev_pts, 500, 0.001, 10, cv::Mat(), 7);
+    cv::Size subPixWinSize(10,10), winSize(21,21);
+    cv::TermCriteria termcrit(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS,20,0.03);
+    
+    features_vis = prev3b.clone();
+    for(size_t i = 0; i < prev_pts.size(); ++i) {
+      cv::circle(features_vis, prev_pts[i], 2, cv::Scalar(0, 0, 255), -1);
+    }
+    cv::imshow("features", features_vis);
+
+    // -- Flow.
+    vector<uchar> status;
+    vector<float> err;
+    cv::calcOpticalFlowPyrLK(prev, curr, prev_pts, curr_pts,
+                             status, err, winSize,
+                             3, termcrit, 0, 0.001);
+
+    flow_vis = prev3b.clone();
+    for(size_t i = 0; i < status.size(); ++i) {
+      if(status[i]) {
+        cv::circle(flow_vis, prev_pts[i], 2, cv::Scalar(0, 0, 255), -1);
+        cv::line(flow_vis, prev_pts[i], curr_pts[i], cv::Scalar(0, 0, 255), 1);
+      }
+    }
+    cv::imshow("flow", flow_vis);
+    
+    // -- Delta image.
+    delta = cv::Mat1b(curr.size(), 0);
+    for(int y = 0; y < curr.rows; ++y)
+      for(int x = 0; x < curr.cols; ++x)
+        delta(y, x) = fabs(curr(y, x) - prev(y, x));
+    cv::imshow("delta", delta);
+    
+    // -- SURF.
+    surf_vis = prev3b.clone();
+    cv::SURF surf(1000);
+    vector<cv::KeyPoint> keypoints;
+    //HighResTimer hrt("surf"); hrt.start();
+    surf(prev3b, cv::Mat(), keypoints);
+    //hrt.stop(); cout << hrt.reportMilliseconds() << endl;
+    for(size_t i = 0; i < keypoints.size(); ++i)
+      cv::circle(surf_vis, keypoints[i].pt, 2, cv::Scalar(0, 255, 0), -1);
+    cv::imshow("surf", surf_vis);
+    
+    char key;
+    key = cv::waitKey(ms);
+    if(key == 'q')
+      break;
+    else if(key == ' ') {
+      if(ms == 0)
+        ms = 5;
+      else
+        ms = 0;
+    }
+  }
+  
+  return 0;
+}
