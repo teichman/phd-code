6725b2474e35bdd34806a1bf3ac09b3788c283ce
Alex Teichman <teichman@cs.stanford.edu>
2013-10-08 02:41:46 -0700

    Dumping out data for nutcracker.

--- ros-pkg/core/nutcracker/src/prog/optical_flow_experiment.cpp
+++ ros-pkg/core/nutcracker/src/prog/optical_flow_experiment.cpp
@@ -27,7 +27,8 @@ int main(int argc, char** argv)
   cv::Mat3b prev3b;
   cv::Mat3b flow_vis;
   cv::Mat3b features_vis;
-  cv::Rect roi(390, 219, 1074, 343);  // On Bankout
+  //cv::Rect roi(390, 219, 1074, 343);  // On Bankout
+  cv::Rect roi(324, 385, 1166, 148);  // On Bankout - smaller crop
   //cv::Rect roi(243, 70, 800, 418);  // Sutter
   cv::Mat1b delta;
   cv::Mat3b surf_vis;
@@ -52,32 +53,32 @@ int main(int argc, char** argv)
     cv::cvtColor(curr3b, curr, CV_BGR2GRAY);
         
     // -- goodFeaturesToTrack.
-    prev_pts.clear();
-    cv::goodFeaturesToTrack(prev, prev_pts, 500, 0.001, 10, cv::Mat(), 7);
-    cv::Size subPixWinSize(10,10), winSize(21,21);
-    cv::TermCriteria termcrit(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS,20,0.03);
+    // prev_pts.clear();
+    // cv::goodFeaturesToTrack(prev, prev_pts, 500, 0.001, 10, cv::Mat(), 7);
+    // cv::Size subPixWinSize(10,10), winSize(21,21);
+    // cv::TermCriteria termcrit(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS,20,0.03);
     
-    features_vis = prev3b.clone();
-    for(size_t i = 0; i < prev_pts.size(); ++i) {
-      cv::circle(features_vis, prev_pts[i], 2, cv::Scalar(0, 0, 255), -1);
-    }
-    cv::imshow("features", features_vis);
+    // features_vis = prev3b.clone();
+    // for(size_t i = 0; i < prev_pts.size(); ++i) {
+    //   cv::circle(features_vis, prev_pts[i], 2, cv::Scalar(0, 0, 255), -1);
+    // }
+    // cv::imshow("features", features_vis);
 
     // -- Flow.
-    vector<uchar> status;
-    vector<float> err;
-    cv::calcOpticalFlowPyrLK(prev, curr, prev_pts, curr_pts,
-                             status, err, winSize,
-                             3, termcrit, 0, 0.001);
+    // vector<uchar> status;
+    // vector<float> err;
+    // cv::calcOpticalFlowPyrLK(prev, curr, prev_pts, curr_pts,
+    //                          status, err, winSize,
+    //                          3, termcrit, 0, 0.001);
 
-    flow_vis = prev3b.clone();
-    for(size_t i = 0; i < status.size(); ++i) {
-      if(status[i]) {
-        cv::circle(flow_vis, prev_pts[i], 2, cv::Scalar(0, 0, 255), -1);
-        cv::line(flow_vis, prev_pts[i], curr_pts[i], cv::Scalar(0, 0, 255), 1);
-      }
-    }
-    cv::imshow("flow", flow_vis);
+    // flow_vis = prev3b.clone();
+    // for(size_t i = 0; i < status.size(); ++i) {
+    //   if(status[i]) {
+    //     cv::circle(flow_vis, prev_pts[i], 2, cv::Scalar(0, 0, 255), -1);
+    //     cv::line(flow_vis, prev_pts[i], curr_pts[i], cv::Scalar(0, 0, 255), 1);
+    //   }
+    // }
+    // cv::imshow("flow", flow_vis);
     
     // -- Delta image.
     delta = cv::Mat1b(curr.size(), 0);
