dbc9b9c8d7b0d097dde1ef35d235cfbc7a17df56
Alex Teichman <teichman@cs.stanford.edu>
2013-10-14 14:48:06 -0700

    [online_learning] Making stat program take multiple arguments and automatically figure out what type of file it is looking at.

--- ros-pkg/core/online_learning/src/program/stat.cpp
+++ ros-pkg/core/online_learning/src/program/stat.cpp
@@ -9,44 +9,54 @@ namespace bfs = boost::filesystem;
 
 int main(int argc, char** argv)
 {
-  // -- Parse args.
+  namespace bpo = boost::program_options;
   bpo::options_description opts_desc("Allowed options");
+  bpo::positional_options_description p;
+
+  vector<string> paths;
   opts_desc.add_options()
     ("help,h", "produce help message")
-    ("gc", bpo::value<string>(), "Grid classifier")
-    ("td", bpo::value<string>(), "Track dataset")
-    ("classify", "Whether to classify the track dataset with the grid classifier")
+    ("paths", bpo::value(&paths)->required()->multitoken(), "List of GridClassifiers and/or TrackDatasets")
     ;
 
+  p.add("paths", -1);
+
   bpo::variables_map opts;
-  bpo::store(bpo::command_line_parser(argc, argv).options(opts_desc).run(), opts);
-  if(opts.count("help")) {
+  bpo::store(bpo::command_line_parser(argc, argv).options(opts_desc).positional(p).run(), opts);
+  bool badargs = false;
+  try { bpo::notify(opts); }
+  catch(...) { badargs = true; }
+  if(opts.count("help") || badargs) {
+    cout << "Usage: " << argv[0] << " [OPTS] PATH [ PATH ... ]" << endl;
+    cout << "  Where PATH is a GridClassifier or TrackDataset." << endl;
+    cout << endl;
     cout << opts_desc << endl;
     return 1;
   }
-  bpo::notify(opts);
 
-  GridClassifier gc;  
-  if(opts.count("gc")) {
-    gc.load(opts["gc"].as<string>());
-    cout << "GridClassifier status: " << endl;
-    cout << gc.status("  ", true) << endl;
-  }
-
-  TrackDataset td;
-  if(opts.count("td")) {
-    td.load(opts["td"].as<string>());
-    cout << "TrackDataset status: " << endl;
-    cout << td.status("  ", true) << endl;
-  }
-
-  if(opts.count("classify")) {
-    ROS_ASSERT(opts.count("gc") && opts.count("td"));
-    for(size_t i = 0; i < td.size(); ++i) {
-      cout << gc.classifyTrack(td[i]).status(gc.nameMapping("cmap")) << endl;
+  for(size_t i = 0; i < paths.size(); ++i) {
+    cout << "============================================================" << endl;
+    cout << "== " << paths[i] << endl;
+    cout << "============================================================" << endl;
+    bfs::path path(paths[i]);
+    if(path.extension().string() == ".gc") {
+      GridClassifier gc;  
+      gc.load(paths[i]);
+      cout << "GridClassifier status: " << endl;
+      cout << gc.status("  ", true) << endl;
     }
+    else if(path.extension().string() == ".td") {
+      TrackDataset td;
+      td.load(paths[i]);
+      cout << "TrackDataset status: " << endl;
+      cout << td.status("  ", true) << endl;
+    }
+    else {
+      cout << "Unknown extension \"" << path.extension().string() << "\"." << endl;
+      cout << "Skipping path " << paths[i] << endl;
+    }
+    cout << endl;
   }
 
-  
   return 0;
 }
