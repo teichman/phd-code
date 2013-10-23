b2db0506ef4a17819b2d2aa1750cd347e1c63549
Alex Teichman <teichman@cs.stanford.edu>
2013-10-22 17:40:24 -0700

    Adding super gimpy but functional method of firing the USB cannon when a cat is detected.

--- ros-pkg/core/jarvis/include/jarvis/cannon_reactor.h
+++ ros-pkg/core/jarvis/include/jarvis/cannon_reactor.h
@@ -1,13 +1,20 @@
 #ifndef REACTOR_CANNON_H
 #define REACTOR_CANNON_H
 
+#include <timer/timer.h>
 #include <jarvis/reactor.h>
 
 class CannonReactor : public Reactor
 {
 public:
   CannonReactor();
+  ~CannonReactor();
+  
+protected:
+  HighResTimer hrt_;
+  
   void detectionCallback(jarvis::DetectionConstPtr msg);
+  void fire() const;
 };
 
 #endif // REACTOR_CANNON_H
--- ros-pkg/core/jarvis/src/lib/cannon_reactor.cpp
+++ ros-pkg/core/jarvis/src/lib/cannon_reactor.cpp
@@ -1,11 +1,33 @@
 #include <jarvis/cannon_reactor.h>
 #include <online_learning/dataset.h>
+#include <X11/Xlib.h>
 
 using namespace std;
 
 CannonReactor::CannonReactor()
 {
+  // -- Start up pyrocket.
+  int ret = system("killall pyrocket");
+  ROS_ASSERT(ret == 0);
+  ret = system("~/pyrocket/src/pyrocket &");
+  ROS_ASSERT(ret == 0);
 
+  // -- Place the mouse cursor over the fire button.
+  Display* display = XOpenDisplay(NULL);
+  assert(display);
+  Window root = DefaultRootWindow(display);
+  int row = 150;
+  int col = 10;
+  XWarpPointer(display, 0, root, 0, 0, 0, 0, col, row);
+  XCloseDisplay(display);
+    
+  hrt_.start();
+}
+
+CannonReactor::~CannonReactor()
+{
+  int ret = system("killall pyrocket");
+  ROS_ASSERT(ret == 0);
 }
 
 void CannonReactor::detectionCallback(jarvis::DetectionConstPtr msg)
@@ -14,4 +36,20 @@ void CannonReactor::detectionCallback(jarvis::DetectionConstPtr msg)
   Label pred(msg->label);
   cout << "Detection: " << endl;
   cout << "  " << pred.status(cmap) << endl;
+
+  if(hrt_.getSeconds() > 5.0) {
+    fire();
+    hrt_.reset();
+    hrt_.start();
+  }
+}
+
+void CannonReactor::fire() const
+{
+  cout << "[CannonReactor]  Fire!" << endl;
+  int ret = system("xdotool mousedown 1");
+  ROS_ASSERT(ret == 0);
+  usleep(1e4);
+  ret = system("xdotool mouseup 1");
+  ROS_ASSERT(ret == 0);
 }
