cb6f7ff7df991654094e115e347a09f38706c03f
Alex Teichman <teichman@cs.stanford.edu>
2013-10-25 01:17:25 -0700

    Messing around with a programmatic interface to pyrocket.

---
+++ ros-pkg/core/jarvis/src/python/rosrocket.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+from rocket_backend import RocketManager
+import thread
+import time
+
+class RocketDriver:
+    def __init__(self):
+        self.rm_ = RocketManager()
+        self.rm_.acquire_devices()
+        assert(len(self.rm_.launchers) == 1)
+        self.launcher_ = self.rm_.launchers[0]
+
+    def left(self, seconds):
+        thread.start_new_thread(self.launcher_.start_movement, (2,))
+        time.sleep(seconds)
+        self.launcher_.stop_movement()
+    
+    def right(self, seconds):
+        thread.start_new_thread(self.launcher_.start_movement, (3,))
+        time.sleep(seconds)
+        self.launcher_.stop_movement()
+
+    def up(self, seconds):
+        thread.start_new_thread(self.launcher_.start_movement, (1,))
+        time.sleep(seconds)
+        self.launcher_.stop_movement()
+
+    def down(self, seconds):
+        thread.start_new_thread(self.launcher_.start_movement, (0,))
+        time.sleep(seconds)
+        self.launcher_.stop_movement()
+
+    def fire(self):
+        self.launcher_.start_movement(4)
+
+
+if __name__ == "__main__":
+    rd = RocketDriver()
+    rd.left(0.3)
+    rd.right(0.3)
+    rd.fire()
