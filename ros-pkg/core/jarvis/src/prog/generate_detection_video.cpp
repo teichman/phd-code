6e0bf86adacd3b58c3258e31e31bfc971d377009
Alex Teichman <teichman@cs.stanford.edu>
2013-11-26 22:02:45 -0800

    First pass at updating RecordingRequester to save all detection metadata into a single file.

--- ros-pkg/core/jarvis/src/prog/generate_detection_video.cpp
+++ ros-pkg/core/jarvis/src/prog/generate_detection_video.cpp
@@ -16,6 +16,50 @@ double logistic(double logodds, double sigma)
   return 1.0 / (1.0 + exp(-logodds / sigma));
 }
 
+struct Det
+{
+  double timestamp_;
+  size_t track_id_;
+  int ulx_;
+  int uly_;
+  int lrx_;
+  int lry_;
+  vector<float> frame_predictions_;
+  vector<string> class_names_;
+};
+
+void load(string path, map<double, vector<Det> >* detections)
+{
+  ifstream file(path.c_str());
+  
+  while(true) {
+    string line;
+    getline(file, line);
+    if(file.eof())
+      break;
+
+    Det det;
+    istringstream iss(line);
+    iss >> det.timestamp_;
+    iss >> det.track_id_;
+    iss >> det.ulx_;
+    iss >> det.uly_;
+    iss >> det.lrx_;
+    iss >> det.lry_;
+
+    while(!iss.eof()) {
+      string class_name;
+      iss >> class_name;
+      float logodds;
+      iss >> logodds;
+      det.frame_predictions_.push_back(logodds);
+      det.class_names_.push_back(class_name);
+    }
+
+    (*detections)[det.timestamp_].push_back(det);
+  }
+}
+
 int main(int argc, char** argv)
 {
   namespace bpo = boost::program_options;
@@ -23,7 +67,7 @@ int main(int argc, char** argv)
   bpo::positional_options_description p;
 
   string images_dir;
-  string detection_metadata_dir;
+  vector<string> detection_metadata_paths;
   string show;
   int pad;
   double sigma;
@@ -32,7 +76,7 @@ int main(int argc, char** argv)
   opts_desc.add_options()
     ("help,h", "produce help message")
     ("images-dir", bpo::value(&images_dir)->required(), "")
-    ("detection-metadata-dir", bpo::value(&detection_metadata_dir)->required(), "")
+    ("detection-metadata-paths", bpo::value(&detection_metadata_paths)->required()->multitoken(), "")
     ("show", bpo::value(&show)->required(), "class to show detections for")
     ("pad", bpo::value(&pad)->default_value(7), "padding on the bounding box")
     ("sigma", bpo::value(&sigma)->default_value(5), "in the logistic function for coloring")
@@ -43,8 +87,8 @@ int main(int argc, char** argv)
     ;
 
   p.add("images-dir", 1);
-  p.add("detection-metadata-dir", 1);
   p.add("show", 1);
+  p.add("detection-metadata-paths", -1);
   
   bpo::variables_map opts;
   bpo::store(bpo::command_line_parser(argc, argv).options(opts_desc).positional(p).run(), opts);
@@ -52,12 +96,19 @@ int main(int argc, char** argv)
   try { bpo::notify(opts); }
   catch(...) { badargs = true; }
   if(opts.count("help") || badargs) {
-    cout << "Usage: " << argv[0] << " [OPTS] IMAGES_DIR DETECTION_METADATA_DIR SHOW" << endl;
+    cout << "Usage: " << argv[0] << " [OPTS] IMAGES_DIR SHOW DETECTION_METADATA [ DETECTION_METADATA ... ]" << endl;
     cout << endl;
     cout << opts_desc << endl;
     return 1;
   }
 
+  // -- Load all detection metadata.
+  map<double, vector<Det> > detections;
+  for(size_t i = 0; i < detection_metadata_paths.size(); ++i) {
+    cout << "Loading " << detection_metadata_paths[i] << endl;
+    load(detection_metadata_paths[i], &detections);
+  }
+  
   string tmpdir = ".generate_detection_video-tmpdir";
   if(bfs::exists(tmpdir))
     bfs::remove_all(tmpdir);
@@ -69,58 +120,33 @@ int main(int argc, char** argv)
   for(size_t i = 0; i < image_paths.size(); ++i) {
     cv::Mat3b img = cv::imread(image_paths[i]);
 
-    // Get the corresponding detection file.
+    // Get the corresponding detection data.
     bfs::path path(image_paths[i]);
     string leaf = path.leaf().string();
     string timestamp_string = leaf.substr(5, leaf.size() - 9);
-    string detection_filename = "detection" + timestamp_string + ".txt";
-    string detection_path = detection_metadata_dir + "/" + detection_filename;
+    double timestamp = atof(timestamp_string.c_str());
 
     // If it exists, draw in the image.
-    if(bfs::exists(detection_path)) {
-      cout << detection_path << endl;
-
-      ifstream file(detection_path.c_str());
-      while(true) {
-        string line;
-        getline(file, line);
-        if(file.eof())
-          break;
-        istringstream iss(line);
-        size_t track_id;
-        iss >> track_id;
-        int ulx, uly, lrx, lry;
-        iss >> ulx;
-        iss >> uly;
-        iss >> lrx;
-        iss >> lry;
-        vector<float> frame_prediction_stl;
-        vector<string> class_names;
-        while(!iss.eof()) {
-          string class_name;
-          iss >> class_name;
-          float logodds;
-          iss >> logodds;
-          frame_prediction_stl.push_back(logodds);
-          class_names.push_back(class_name);
-        }
-
+    if(detections.count(timestamp)) {
+      vector<Det> imgdet = detections[timestamp];
+      for(size_t j = 0; j < imgdet.size(); ++j) {
+        Det det = imgdet[j];
+        
         // -- Update DiscreteBayesFilter.  Use fake centroid motion because we don't have
         //    that info anymore.
-        Label fpred(frame_prediction_stl);
-        double timestamp = atof(timestamp_string.c_str());
+        Label fpred(det.frame_predictions_);
         VectorXf centroid = VectorXf::Ones(3); 
         centroid *= timestamp;
-        filters[track_id].addObservation(fpred, centroid, timestamp);
-        NameMapping cmap(class_names);
+        filters[det.track_id_].addObservation(fpred, centroid, timestamp);
+        NameMapping cmap(det.class_names_);
         
         // -- Draw.
-        Label tpred = filters[track_id].trackPrediction();
+        Label tpred = filters[det.track_id_].trackPrediction();
         float logodds = tpred(cmap.toId(show));
         if(opts.count("frame"))
           logodds = fpred(cmap.toId(show));
-        cv::Point ul(max(0, ulx - pad), max(0, uly - pad));
-        cv::Point lr(min(lrx + pad, img.cols), min(lry + pad, img.rows));
+        cv::Point ul(max(0, det.ulx_ - pad), max(0, det.uly_ - pad));
+        cv::Point lr(min(det.lrx_ + pad, img.cols), min(det.lry_ + pad, img.rows));
         cv::Scalar color(127, 127, 127);
         if(logodds > 0) {
           float val = 255 * logistic(logodds, sigma);
--- ros-pkg/core/jarvis/src/prog/request_detection_recording.cpp
+++ ros-pkg/core/jarvis/src/prog/request_detection_recording.cpp
@@ -14,6 +14,7 @@ class RecordingRequester
 public:
   RecordingRequester(std::string class_name, double threshold,
                      std::string metadata_path, double seconds = 1);
+  ~RecordingRequester();
   
 protected:
   ros::NodeHandle nh_;
@@ -21,9 +22,9 @@ protected:
   ros::Subscriber sub_;
   std::string class_name_;
   double threshold_;
-  //! Where to store detection<timestamp>.txt.
   std::string metadata_path_;
   double seconds_;
+  ofstream output_;
 
   void detectionCallback(const jarvis::Detection& msg);
 };
@@ -38,8 +39,13 @@ RecordingRequester::RecordingRequester(std::string class_name, double threshold,
   pub_ = nh_.advertise<sentinel::RecordingRequest>("recording_requests", 0);
   sub_ = nh_.subscribe("detections", 0, &RecordingRequester::detectionCallback, this);
 
-  if(!bfs::exists(metadata_path_))
-    bfs::create_directory(metadata_path_);
+  ROS_ASSERT(!bfs::exists(metadata_path_));
+  output_.open(metadata_path_.c_str());
+}
+
+RecordingRequester::~RecordingRequester()
+{
+  output_.close();
 }
 
 void RecordingRequester::detectionCallback(const jarvis::Detection& det)
@@ -65,17 +71,13 @@ void RecordingRequester::detectionCallback(const jarvis::Detection& det)
 
   // -- Record metadata to a text file.  We'll use this to generate a video with bounding boxes.
   //    (All classifications are sent within the Detection messages, not just positive detections.)
-  ostringstream oss;
-  oss << metadata_path_ << "/detection" << fixed << setprecision(16) << setw(16) << setfill('0') << det.header.stamp.toSec() << ".txt";
-  ofstream file;
-  file.open(oss.str().c_str(), std::fstream::app);
-  file << det.track_id << " " 
-       << det.upper_left.x << " " << det.upper_left.y << " "
-       << det.lower_right.x << " " << det.lower_right.y;
+  output_ << fixed << setprecision(16) << setw(16) << setfill('0') << det.header.stamp.toSec() << " "
+          << det.track_id << " " 
+          << det.upper_left.x << " " << det.upper_left.y << " "
+          << det.lower_right.x << " " << det.lower_right.y;
   for(size_t i = 0; i < det.frame_prediction.size(); ++i)
-    file << " " << det.cmap[i] << " " << det.frame_prediction[i];
-  file << endl;
-  file.close();
+    output_ << " " << det.cmap[i] << " " << det.frame_prediction[i];
+  output_ << endl;
 }
 
 int main(int argc, char** argv)
@@ -88,19 +90,19 @@ int main(int argc, char** argv)
 
   string class_name;
   double threshold;
-  string metadata_path;
+  string metadata_dir;
   double seconds;
   opts_desc.add_options()
     ("help,h", "produce help message")
     ("class-name,c", bpo::value(&class_name)->required(), "")
     ("threshold,t", bpo::value(&threshold)->required(), "")
-    ("metadata-path,p", bpo::value(&metadata_path)->required(), "Where to store detection<timestamp>.txt files.")
+    ("metadata-dir,d", bpo::value(&metadata_dir)->required(), "Where to store the detections file.  Should be a directory that exists.  The file will be added to it.")
     ("seconds,s", bpo::value(&seconds)->default_value(3), "Number of seconds to record after no more detections are seen.")
     ;
 
   p.add("class-name", 1);
   p.add("threshold", 1);
-  p.add("metadata-path", 1);
+  p.add("metadata-dir", 1);
   p.add("seconds", 1);
 
   bpo::variables_map opts;
@@ -109,13 +111,15 @@ int main(int argc, char** argv)
   try { bpo::notify(opts); }
   catch(...) { badargs = true; }
   if(opts.count("help") || badargs) {
-    cout << "Usage: " << argv[0] << " [OPTS] CLASS_NAME THRESHOLD METADATA_PATH SECONDS" << endl;
+    cout << "Usage: " << argv[0] << " [OPTS] CLASS_NAME THRESHOLD METADATA_DIR SECONDS" << endl;
     cout << endl;
     cout << opts_desc << endl;
     return 1;
   }
 
-  RecordingRequester rr(class_name, threshold, metadata_path, seconds);
+  ostringstream oss;
+  oss << metadata_dir << "/detections_metadata-" << fixed << setprecision(16) << setw(16) << setfill('0') << ros::Time::now().toSec() << ".txt";
+  RecordingRequester rr(class_name, threshold, oss.str(), seconds);
   ros::spin();
 
   return 0;
