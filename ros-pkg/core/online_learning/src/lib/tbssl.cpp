6354e7be2cf314f502f329fbf5951e18b557034c
Alex Teichman <teichman@cs.stanford.edu>
2014-01-06 17:04:18 -0800

    The first 20 annotations do not go to validation_.

--- ros-pkg/core/online_learning/src/lib/tbssl.cpp
+++ ros-pkg/core/online_learning/src/lib/tbssl.cpp
@@ -195,7 +195,7 @@ void OnlineLearner::handleAnnotatedData()
   TrackDataset for_annotated;
   for_annotated.applyNameMappings(aggregated);
   for(size_t i = 0; i < aggregated.size(); ++i, ++ann_counter_) {
-    if(ann_counter_ > 10 && ann_counter_ % 3 == 0) {
+    if(ann_counter_ > 20 && ann_counter_ % 3 == 0) {
       validation_->tracks_.push_back(aggregated.tracks_[i]);
     }
     else {
