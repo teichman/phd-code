3802a3063d24191cd8f19e7a6316878e25b334ce
Hendrik Dahlkamp <dahlkamp@gmail.com>
2014-04-12 12:34:58 -0700

    ClusterListView for scrolling thru clusters.

--- ros-pkg/core/jarvis/include/jarvis/cluster_view_controller.h
+++ ros-pkg/core/jarvis/include/jarvis/cluster_view_controller.h
@@ -1,37 +1,82 @@
 #ifndef CLUSTER_VIEW_CONTROLLER_H
 #define CLUSTER_VIEW_CONTROLLER_H
 
-#include <online_learning/track_dataset_visualizer.h>
+#include <vector>
+
+//#include <online_learning/track_dataset_visualizer.h>
+#include <agent/agent.h>
 #include <jarvis/view_controller.h>
 
 using boost::shared_ptr;
 
 class Dataset;
 class GridClassifier;
-class OnlineLearner;
 class Texture;
 class TrackDataset;
+class OnlineLearner;
 
-// View that displays Blob clusters.
-class ClusterBlobView : public ViewController, public ClusterView
+class ClusterView
 {
 public:
-  typedef shared_ptr<ClusterBlobView> Ptr;
-  ClusterBlobView();
+  typedef shared_ptr<ClusterView> Ptr;
+  ClusterView(boost::shared_ptr<TrackDataset> cluster);
+  virtual ~ClusterView() {}
 
-  virtual void displayCluster(shared_ptr<TrackDataset> td);
-  virtual void displayMessage(const std::string& message);
-
+  // renders the cluster into unit rectangle
   virtual void display();
 
-//  virtual void mouse(int button, int state, int x, int y) {}
-//  virtual void motion(int x, int y) {}
-  virtual void key(unsigned char k, int x, int y);
-
 protected:
-  shared_ptr<TrackDataset> td_;
-  size_t instance_displayed_;
+  shared_ptr<TrackDataset> cluster_;
+  size_t instance_displayed_, max_instances_;
 };
 
+
+class ClusterListView : public SharedLockable, public ViewController {
+public:
+  ClusterListView()
+      : scroll_position_(0), scroll_momentum_(0), dragging_(false) {}
+  virtual ~ClusterListView() {}
+
+  virtual void display();
+  virtual void motion(int x, int y);
+  virtual void mouse(int button, int button_state, int x, int y);
+
+  void clear();
+  // number of clusters user can scroll down before reaching the end of list
+  // can be <=0 if clusters are urgently needed
+  int clustersBeforeEnd();
+  virtual void displayCluster(boost::shared_ptr<TrackDataset> cluster);
+
+  // returns index into clusters_ that user clicked on (-1 for none)
+  int indexPicked(int mouse_x, int mouse_y);
+
+protected:
+  std::vector<ClusterView::Ptr> clusters_;
+  float scroll_position_;
+  float scroll_momentum_;  // clusters/sec
+  double scroll_last_timestep_;
+  bool dragging_;
+  int last_mouse_y_;
+};
+
+
+class ClusterListVC : public ClusterListView, public Agent {
+public:
+  typedef shared_ptr<ClusterListVC> Ptr;
+  ClusterListVC() : learner_(NULL) {}
+  virtual ~ClusterListVC() {}
+
+  void setOnlineLearner(OnlineLearner* learner) { learner_ = learner; }
+
+  void addAllSimilarTo(boost::shared_ptr<Dataset> reference,
+                       boost::shared_ptr<TrackDataset> td,
+                       const GridClassifier &gc);
+  virtual void mouse(int button, int state, int x, int y);
+
+  void _run();
+
+protected:
+  OnlineLearner* learner_;
+};
 
 #endif // CLUSTER_VIEW_CONTROLLER_H
