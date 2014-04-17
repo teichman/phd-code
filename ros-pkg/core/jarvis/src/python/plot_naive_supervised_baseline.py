008394cf99dddece27e5983310c3b1272542a39c
Alex Teichman <teichman@cs.stanford.edu>
2014-04-17 13:30:13 -0700

    Switching nsb plots to percent.

--- ros-pkg/core/jarvis/src/python/plot_naive_supervised_baseline.py
+++ ros-pkg/core/jarvis/src/python/plot_naive_supervised_baseline.py
@@ -28,15 +28,13 @@ cp.setup()
 # -- Read in the pairs of (num_tr_ex, acc).
 subsample_names = loadBash("find -type d -name 'subsample*' | sort | awk -F'/' '{print $NF}' | sort | uniq")
 print subsample_names
-#ntrs = npLoadBash("grep -h '[0-9] tracks\.' `find -name training_stats.txt | grep naive_supervised_baseline | sort` | awk '{print $1}'")
-#accs = npLoadBash("grep 'Total acc' `find -name track_results.txt | grep naive_supervised_baseline | grep quantity_experiment | sort` | awk '{print $NF}'")
 
 means = np.zeros((len(subsample_names)))
 stderrs = np.zeros((len(subsample_names)))
 ntrs = np.zeros((len(subsample_names)))
 
 for idx, subsample_name in enumerate(subsample_names):
-    accs = npLoadBash("grep 'Total acc' `find -name track_results.txt | grep naive_supervised_baseline | grep quantity | grep " + subsample_name + " | sort` | awk '{print $NF}'")
+    accs = 100 * npLoadBash("grep 'Total acc' `find -name track_results.txt | grep naive_supervised_baseline | grep quantity | grep " + subsample_name + " | sort` | awk '{print $NF}'")
     ntr = npLoadBash("grep -h '[0-9] tracks\.' `find -name training_stats.txt | grep " + subsample_name + " | sort` | awk '{print $1}' | sort | uniq")
     assert(ntr.size == 1)  # All runs in each subsample should have the same number of tracks.
 
@@ -46,7 +44,7 @@ for idx, subsample_name in enumerate(subsample_names):
 
 
 # -- Read in the accuracies of group induction.
-gi_accs = npLoadBash("grep 'Total acc' `find -L -wholename '*/induction/final_track_results.txt' | sort` | awk '{print $NF}'")
+gi_accs = 100 * npLoadBash("grep 'Total acc' `find -L -wholename '*/induction/final_track_results.txt' | sort` | awk '{print $NF}'")
 gi_ntrs = npLoadBash("for dir in `find -L -maxdepth 1 -mindepth 1 -type d`; do grep -A2 'Hand-annotated' `find $dir -wholename '*/induction/*/learner_status.txt' | sort | tail -n1`; done | grep tracks | awk '{print $1}'")
 
 print gi_accs
@@ -62,7 +60,7 @@ ax.errorbar(ntrs, means, yerr=stderrs, fmt='-o', color='black', markersize=5, li
 ax.plot(gi_ntrs, gi_accs, 'x', color='red', markersize=6, markeredgewidth=2, label='Group induction')
 
 plt.xlabel('Number of supervised training examples')
-plt.ylabel('Accuracy')
+plt.ylabel('Accuracy (\%)')
 plt.title(getPlotTitle())
 
 plt.legend(loc='lower right')
