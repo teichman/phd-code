d8f6f58ce9976c80ea8511f0c41bf81c6c452756
Alex Teichman <teichman@cs.stanford.edu>
2014-06-13 15:50:53 -0700

    Cleaning up track_times.cpp

--- ros-pkg/core/jarvis/src/prog/track_times.cpp
+++ ros-pkg/core/jarvis/src/prog/track_times.cpp
@@ -14,32 +14,32 @@ int main(int argc, char** argv)
   bpo::options_description opts_desc("Allowed options");
   bpo::positional_options_description p;
 
-  string input;
+  vector<string> td_paths;
   opts_desc.add_options()
     ("help,h", "produce help message")
-    ("input", bpo::value(&input)->required(), "Input tds directory.");
+    ("tds", bpo::value(&td_paths)->required()->multitoken(), "Input tds.");
 
-  p.add("input", 1);
+  p.add("tds", -1);
+  
   bpo::variables_map opts;
   bpo::store(bpo::command_line_parser(argc, argv).options(opts_desc).positional(p).run(), opts);
   bool badargs = false;
   try { bpo::notify(opts); }
   catch(...) { badargs = true; }
   if(opts.count("help") || badargs) {
-    cout << "Usage: " << argv[0] << " [OPTS] TD" << endl;
+    cout << "Usage: " << argv[0] << " [OPTS] TD [ TD ... ]" << endl;
     cout << endl;
     cout << opts_desc << endl;
     return 1;
   }
 
   bpt::ptime epoch(boost::gregorian::date(1970,1,1));
-  bfs::directory_iterator it(input), eod;
-  BOOST_FOREACH(bfs::path const & p, make_pair(it, eod)){
+  for(size_t i = 0; i < td_paths.size(); ++i) {
     TrackDataset td;
-    td.load(p.string());
+    td.load(td_paths[i]);
     const Dataset& lt = td[td.size()-1];
     ulong t = boost::any_cast<Blob::ConstPtr>(td[0][0].raw())->wall_timestamp_.toSec();
-    cout << p.string() << endl;
+    cout << td_paths[i] << endl;
     cout << "\t" << bpt::to_iso_string(bpt::from_time_t(t)) << endl;
     t = boost::any_cast<Blob::ConstPtr>(lt[lt.size()-1].raw())->wall_timestamp_.toSec();
     cout << "\t" << bpt::to_iso_string(bpt::from_time_t(t)) << endl;
